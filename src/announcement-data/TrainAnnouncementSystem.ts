5b8d86bc239003c25fe2bd0c1ffccbbd76238372
David Wheatley <hi@davwheat.dev>
2021-08-28 22:11:22 +0100

    Add helper functions

--- src/announcement-data/TrainAnnouncementSystem.ts
+++ src/announcement-data/TrainAnnouncementSystem.ts
@@ -1,4 +1,5 @@
-import AnnouncementSystem from './AnnouncementSystem'
+import { getStationByCrs } from '@data/StationManipulators'
+import AnnouncementSystem, { AudioItem } from './AnnouncementSystem'
 
 interface IAnyOptions {
   [key: string]: any
@@ -46,4 +47,26 @@ export default abstract class TrainAnnouncementSystem extends AnnouncementSystem
   abstract readonly stoppedAtStationAnnouncementOptions: Record<string, IOptionsExplanation>
 
   abstract readonly AvailableStationNames: { high: string[]; low: string[] }
+
+  protected validateStationExists(stationCrs: string, type: 'high' | 'low'): boolean {
+    if (!this.AvailableStationNames[type].includes(stationCrs)) {
+      const name = getStationByCrs(stationCrs).stationName
+      console.error(`No audio file could be found for ${stationCrs} (${name}) in the pitch required for this announcement (${type})!`)
+
+      alert(
+        `Unfortunately, we don't have the recording for ${name} in the needed type (type: ${type}). If you can record this, please do so, then create an issue or PR on GitHub!`,
+      )
+      return false
+    }
+    return true
+  }
+
+  protected pluraliseAudio(...items: AudioItem[]): AudioItem[] {
+    if (items.length > 1) {
+      items.splice(items.length - 1, 0, 'and')
+      return items
+    }
+
+    return items
+  }
 }

ed0a8f6e144ff4263549825626307bb8476abaca
David Wheatley <hi@davwheat.dev>
2021-08-28 18:29:57 +0100

    Update 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from '@components/Layout'
+import { Link } from 'gatsby'
+
+function Error404Page({ location }): JSX.Element {
+  return (
+    <Layout title="Rail announcements" location={location}>
+      <header>
+        <h1>Error 404 - Not found</h1>
+      </header>
+      <main>
+        <p>Oh no! Looks like this page couldn't be found.</p>
+        <Link to="/">Go to home page</Link>
+      </main>
+    </Layout>
+  )
+}
+
+export default Error404Page

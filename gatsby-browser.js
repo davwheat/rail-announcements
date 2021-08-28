6c562780b4cde3518f06111bfa1fc8f19eb7dfbc
David Wheatley <hi@davwheat.dev>
2021-08-28 22:16:55 +0100

    Remove debug code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,4 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import TLClass700 from '@announcement-data/TLClass700'
 import './src/styles/main.less'
-
-window.__700 = new TLClass700()

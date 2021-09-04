bade8abf728edd81ddf727fbde82b4cd5b5c1f19
David Wheatley <hi@davwheat.dev>
2021-09-04 02:49:44 +0100

    feat(station announcement system): add method to show alert for missing files

--- src/announcement-data/StationAnnouncementSystem.ts
+++ src/announcement-data/StationAnnouncementSystem.ts
@@ -1,3 +1,13 @@
 import AnnouncementSystem from './AnnouncementSystem'
 
-export default abstract class StationAnnouncementSystem extends AnnouncementSystem {}
+export default abstract class StationAnnouncementSystem extends AnnouncementSystem {
+  /**
+   * Shows an alert to the user.
+   */
+  protected showAudioNotExistsError(fileName: string): void {
+    console.error(`No audio file could be found for \`${fileName}\`!`)
+    alert(
+      `Unfortunately, we don't have the audio needed for this (${fileName}). If you can record this, please do so, then create an issue or PR on GitHub!`,
+    )
+  }
+}

651b72163eaf2c7b22e5aff742de8b4cd9e4f978
David Wheatley <hi@davwheat.dev>
2021-09-07 12:26:38 +0100

    feat(atos matt): add delay announcement

--- src/announcement-data/systems/stations/AtosMatt.ts
+++ src/announcement-data/systems/stations/AtosMatt.ts
@@ -20,6 +20,16 @@ interface IThroughTrainAnnouncementOptions {
   platform: string
 }
 
+interface IDelayedTrainAnnouncementOptions {
+  hour: string
+  min: string
+  toc: string
+  terminatingStationCode: string
+  via: string | 'none'
+  delayTime: string
+  disruptionReason: string
+}
+
 const AVAILABLE_HOURS = ['07', '08', '13']
 const AVAILABLE_MINUTES = ['03', '08', '27', '33', '36', '53', '57']
 const AVAILABLE_TOCS = ['Southern', 'Thameslink']
@@ -206,6 +216,38 @@ export default class AtosMatt extends StationAnnouncementSystem {
     await this.playAudioFiles(files, download)
   }
 
+  private async playDelayedTrainAnnouncement(options: IDelayedTrainAnnouncementOptions, download: boolean = false): Promise<void> {
+    const { delayTime, disruptionReason } = options
+    const files: AudioItem[] = []
+
+    if (
+      !this.validateOptions({
+        hour: options.hour,
+        minute: options.min,
+        toc: options.toc,
+        number: delayTime !== 'unknown' ? delayTime : undefined,
+        disruptionReason: disruptionReason !== 'unknown' ? disruptionReason : undefined,
+      })
+    )
+      return
+
+    files.push('we are sorry that the', ...this.assembleTrainInfo({ ...options, destAllHigh: true }))
+
+    if (delayTime === 'unknown') {
+      alert("We can't handle unknown delays yet as we're missing some audio recordings.")
+      return
+      // files.push('is being delayed', 'please listen for further announcements')
+    } else {
+      files.push('is delayed by approximately', `numbers.${delayTime}`, 'minutes')
+    }
+
+    if (disruptionReason !== 'unknown') {
+      files.push('this is due to', `disruption-reasons.${disruptionReason}`)
+    }
+
+    await this.playAudioFiles(files, download)
+  }
+
   private validateOptions({
     stationsHigh,
     stationsLow,
@@ -341,6 +383,57 @@ export default class AtosMatt extends StationAnnouncementSystem {
             name: 'Platform',
             default: AVAILABLE_PLATFORMS.low.filter(x => AVAILABLE_PLATFORMS.high.includes(x))[0],
             options: AVAILABLE_PLATFORMS.low.filter(x => AVAILABLE_PLATFORMS.high.includes(x)).map(p => ({ title: `Platform ${p}`, value: p })),
+            type: 'select',
+          },
+        },
+      },
+    },
+    delayedTrain: {
+      name: 'Delayed train',
+      component: CustomAnnouncementPane,
+      props: {
+        playHandler: this.playDelayedTrainAnnouncement.bind(this),
+        options: {
+          hour: {
+            name: 'Hour',
+            default: AVAILABLE_HOURS[0],
+            options: AVAILABLE_HOURS.map(h => ({ title: h, value: h })),
+            type: 'select',
+          },
+          min: {
+            name: 'Minute',
+            default: AVAILABLE_MINUTES[0],
+            options: AVAILABLE_MINUTES.map(m => ({ title: m, value: m })),
+            type: 'select',
+          },
+          toc: {
+            name: 'TOC',
+            default: AVAILABLE_TOCS[0],
+            options: AVAILABLE_TOCS.map(m => ({ title: m, value: m.toLowerCase() })),
+            type: 'select',
+          },
+          terminatingStationCode: {
+            name: 'Terminating station',
+            default: AVAILABLE_STATIONS.high[0],
+            options: AllStationsTitleValueMap.filter(s => AVAILABLE_STATIONS.high.includes(s.value)),
+            type: 'select',
+          },
+          via: {
+            name: 'Via... (optional)',
+            default: 'none',
+            options: [{ title: 'NONE', value: 'none' }, ...AllStationsTitleValueMap.filter(s => AVAILABLE_STATIONS.high.includes(s.value))],
+            type: 'select',
+          },
+          delayTime: {
+            name: 'Delay time',
+            default: AVAILABLE_NUMBERS[0],
+            options: [{ title: 'Unknown', value: 'unknown' }, ...AVAILABLE_NUMBERS.map(h => ({ title: `${h} minute(s)`, value: h }))],
+            type: 'select',
+          },
+          disruptionReason: {
+            name: 'Delay reason',
+            default: 'unknown',
+            options: [{ title: 'Unknown', value: 'unknown' }, ...AVAILABLE_DISRUPTION_REASONS.map(h => ({ title: h, value: h.toLowerCase() }))],
             type: 'select',
           },
         },

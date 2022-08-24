796bb370eb7b3560e561626a7120076701d40910
David Wheatley <hi@davwheat.dev>
2022-08-24 17:57:45 +0200

    feat: support test track stations

--- src/components/AnnouncementPanel.tsx
+++ src/components/AnnouncementPanel.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { makeStyles } from '@material-ui/styles'
 import Tabs from './Tabs'
 import getActiveSystem from '@helpers/getActiveSystem'
 import AnnouncementTabErrorBoundary from './AnnouncementTabErrorBoundary'
+
+import useStateWithLocalStorage from '@hooks/useStateWithLocalStorage'
 
 const useStyles = makeStyles({
   root: {
@@ -35,13 +37,41 @@ function AnnouncementPanel(): JSX.Element {
     [customTabs, AnnouncementSystem],
   )
 
+  const [_selectedTab, _setSelectedTab] = useStateWithLocalStorage<Record<string, number>>('selectedSystemTabs', {}, tabs => {
+    if (typeof tabs !== 'object' || tabs === null) return false
+
+    if (!Object.values(tabs).every(i => typeof i === 'number')) return false
+
+    return true
+  })
+
+  function getSelectedTab() {
+    return _selectedTab?.[AnnouncementSystemInstance.ID] || 0
+  }
+
+  const setSelectedTab = useCallback(
+    (index: number) => {
+      _setSelectedTab(s => ({
+        ...s,
+        [AnnouncementSystemInstance.ID]: index,
+      }))
+    },
+    [_setSelectedTab],
+  )
+
   if (!AnnouncementSystem) return null
 
   return (
     <div className={classes.root}>
       <h2 className={classes.heading}>{AnnouncementSystemInstance.NAME}</h2>
 
-      <Tabs tabNames={Object.values(customTabs).map(tab => tab.name)} tabItems={TabPanels} customKeyPrefix={AnnouncementSystemInstance.ID} />
+      <Tabs
+        selectedTabIndex={getSelectedTab()}
+        onTabChange={setSelectedTab}
+        tabNames={Object.values(customTabs).map(tab => tab.name)}
+        tabItems={TabPanels}
+        customKeyPrefix={AnnouncementSystemInstance.ID}
+      />
     </div>
   )
 }

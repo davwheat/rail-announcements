a9986d7f70cce9d1bd4a7525b6b2a875c0be3b0b
David Wheatley <hi@davwheat.dev>
2022-10-06 00:57:27 +0100

    fix: special chars showing due to BR font

--- src/announcement-data/systems/stations/AtosAnne.tsx
+++ src/announcement-data/systems/stations/AtosAnne.tsx
@@ -535,7 +535,7 @@ export default class AtosAnne extends StationAnnouncementSystem {
           seating: {
             name: 'Seating availability',
             default: 'none',
-            options: [{ title: '<not stated>', value: 'none' }, ...AVAILABLE_SEATING_AVAILABILITY.map(c => ({ title: c, value: c }))],
+            options: [{ title: '(not stated)', value: 'none' }, ...AVAILABLE_SEATING_AVAILABILITY.map(c => ({ title: c, value: c }))],
             type: 'select',
           },
           special: {

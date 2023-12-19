a93802c8fc8410caec778e4351abc3f6c26eed18
David Wheatley <david@davwheat.dev>
2023-12-19 21:14:30 +0000

    feat: rs2 for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#000`,
         theme_color: `#ffa500`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/rs2.png`,
       },
     },
     {

6b3d0c13f8a21b016378c1b7597421e46f887c4a
David Wheatley <david@davwheat.dev>
2023-12-19 21:14:18 +0000

    feat: better svg asset handling

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,5 +34,13 @@ module.exports = {
     {
       resolve: '@sentry/gatsby',
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.inline\.svg$/,
+        },
+      },
+    },
   ],
 }

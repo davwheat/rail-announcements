e829e64476846482f770b63e3cbad29b5ec4b849
David Wheatley <david@davwheat.dev>
2023-12-20 02:19:56 +0000

    feat: workers api cors

--- cf-workers/src/index.ts
+++ cf-workers/src/index.ts
@@ -1,4 +1,7 @@
 import { Router } from 'itty-router'
+import { error, missing } from 'itty-router-extras'
+import { createCors } from 'itty-cors'
+
 import { saveAnnouncementHandler } from './save-announcement'
 import { getAnnouncementHandler } from './get-announcement'
 
@@ -6,15 +9,23 @@ export interface Env {
   DB: D1Database
 }
 
+const { preflight, corsify } = createCors({ origins: ['*'] })
+
 const router = Router()
+
+router.all('*', preflight)
 
 router.post('/save-announcement', saveAnnouncementHandler)
 router.get('/get-announcement', getAnnouncementHandler)
 
-router.all('*', () => new Response('', { status: 404 }))
+router.all('*', missing)
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return router.handle(request, env, ctx)
+    return router
+      .handle(request, env, ctx)
+      .then(corsify)
+      .catch(err => error(500, err.stack))
+      .then(corsify)
   },
 }

1cc81bae49e5ec92dd31cb2e66b84ac53f09f731
David Wheatley <david@davwheat.dev>
2024-01-14 18:22:47 +0000

    feat: extract disclaimers

---
+++ src/components/Disclaimers.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  noticeBlock: {
+    marginTop: 24,
+    padding: '24px 16px',
+    background: '#eee',
+
+    '& > :last-child': {
+      marginBottom: 0,
+    },
+
+    '& > h2:not(:first-child)': {
+      marginTop: 16,
+    },
+  },
+})
+
+interface DisclaimersProps {
+  customDisclaimers?: React.ReactNode[]
+}
+
+export default function Disclaimers({ customDisclaimers = [] }: DisclaimersProps) {
+  const classes = useStyles()
+
+  return (
+    <>
+      <aside key="base" className={classes.noticeBlock}>
+        <h2>Attribution and copyright</h2>
+        <p>
+          Content on this site has either been self-recorded, or released into the public domain via Freedom of Information requests. Copyright
+          may still apply to these files, and may be held by either the respective TOCs, PIS/CIS manufacturers or even the voice artists.
+        </p>
+        <p>
+          You <u>must not</u> use the content on this site for commercial purposes. These files and systems are provided for personal use only in
+          an attempt to archive present-day and historical PIS/CIS systems. Abuse of the website and its contents could result in legal action
+          against you by copyright holders, and/or cause this site to be taken down.
+        </p>
+
+        <h2>Health and safety</h2>
+        <p>
+          This website is provided for educations and entertainment purposes only. It may not be used as a replacement for a public customer
+          information system or be used as a source of accurate real-time data.
+        </p>
+      </aside>
+
+      {customDisclaimers.map((disclaimer, index) => (
+        <aside key={index} className={classes.noticeBlock}>
+          {disclaimer}
+        </aside>
+      ))}
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import MainSelector from '@components/MainSelector'
 import Layout from '@components/Layout'
 import AnnouncementPanel from '@components/AnnouncementPanel'
 import NavBar from '@components/NavBar'
+import Disclaimers from '@components/Disclaimers'
 
 import RailSymbol from '@assets/rail-symbol-2/white-on-red-inset.svg'
 
@@ -23,22 +24,6 @@ const useStyles = makeStyles({
     width: 64,
     marginRight: 16,
     marginTop: -3,
-  },
-  atosNotice: {
-    background: '#fee',
-  },
-  noticeBlock: {
-    marginTop: 24,
-    padding: '24px 16px',
-    background: '#eee',
-
-    '& > :last-child': {
-      marginBottom: 0,
-    },
-
-    '& > h2:not(:first-child)': {
-      marginTop: 16,
-    },
   },
 })
 
@@ -62,30 +47,13 @@ function IndexPage({ location }): JSX.Element {
         <AnnouncementPanel />
       </main>
 
-      <aside className={clsx(classes.noticeBlock, classes.atosNotice)}>
-        <p>
-          Files relating to the Atos Worldline system have been removed from this site. <Link to="/atos-worldline">Learn more</Link>
-        </p>
-      </aside>
-
-      <aside className={classes.noticeBlock}>
-        <h2>Attribution and copyright</h2>
-        <p>
-          Content on this site has either been self-recorded, or released into the public domain via Freedom of Information requests. Copyright
-          may still apply to these files, and may be held by either the respective TOCs, PIS/CIS manufacturers or even the voice artists.
-        </p>
-        <p>
-          You <u>must not</u> use the content on this site for commercial purposes. These files and systems are provided for personal use only in
-          an attempt to archive present-day and historical PIS/CIS systems. Abuse of the website and its contents could result in legal action
-          against you by copyright holders, and/or cause this site to be taken down.
-        </p>
-
-        <h2>Health and safety</h2>
-        <p>
-          This website is provided for educations and entertainment purposes only. It may not be used as a replacement for a public customer
-          information system or be used as a source of accurate real-time data.
-        </p>
-      </aside>
+      <Disclaimers
+        customDisclaimers={[
+          <p>
+            Files relating to the Atos Worldline system have been removed from this site. <Link to="/atos-worldline">Learn more</Link>
+          </p>,
+        ]}
+      />
     </Layout>
   )
 }

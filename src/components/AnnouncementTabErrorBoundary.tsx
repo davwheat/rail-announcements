d411ee7eaec84f5fcc136b9ed12e76ada479c9b9
David Wheatley <david@davwheat.dev>
2024-01-15 23:04:17 +0000

    feat: additional debug info for errors

--- src/components/AnnouncementTabErrorBoundary.tsx
+++ src/components/AnnouncementTabErrorBoundary.tsx
@@ -18,20 +18,25 @@ export default function AnnouncementTabErrorBoundary(props: IAnnouncementTabErro
         })
         scope.setContext('audio ids', { ids: window.__audio })
       }}
-      fallback={({ eventId, resetError }) => (
+      fallback={({ eventId, resetError, error, componentStack }) => (
         <div>
-          <p>Uh oh, something went wrong while playing this announcement!</p>
+          <p>Uh oh, something went wrong while playing this announcement.</p>
+          <p>If you use an ad blocking system, please disable this and try to reproduce the error.</p>
           <p>
-            If this happens again, message{' '}
-            <a
-              href={`https://twitter.com/messages/compose?recipient_id=1033075771659747329&text=${encodeURIComponent(
-                'I had an issue while creating an announcement. Event ID: ' + eventId,
-              )}`}
-            >
-              <code>@davwheat_</code>
-            </a>{' '}
-            on Twitter or open a GitHub issue, and quote event ID: <code>{eventId}</code>
+            If this happens again, <a href="https://github.com/davwheat/rail-announcements/issues/new">open a GitHub issue</a>, and quote event
+            ID <code>{eventId}</code> <strong>and</strong> provide the detailed error information below.
           </p>
+          <details style={{ margin: '16px 0' }}>
+            <summary>Detailed info</summary>
+
+            <textarea
+              style={{ width: '100%' }}
+              rows={8}
+              readOnly
+              value={`${error?.name}: ${error.message}\n\n${error.stack || '<no stacktrace>'}`}
+            />
+            <textarea style={{ width: '100%' }} rows={25} readOnly value={componentStack} />
+          </details>
           <p>
             <button onClick={() => resetError()}>Try again</button>
           </p>

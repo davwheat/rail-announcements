6f4aae9200174a330c1563be68030a59baefbf01
David Wheatley <david.wheatley@raileasy.co.uk>
2024-07-08 21:17:50 +0100

    fix: wrong short platform "customers for"

--- src/announcement-data/systems/stations/AmeyPhil.tsx
+++ src/announcement-data/systems/stations/AmeyPhil.tsx
@@ -4123,7 +4123,7 @@ export default class AmeyPhil extends StationAnnouncementSystem {
           {
             // If last set of short platforms, add 'and' before last item
             // See: https://github.com/davwheat/rail-announcements/issues/226#issuecomment-2212472715
-            id: i === order.length - 1 ? 'm.and customers for' : 's.customers for',
+            id: i === order.length - 1 ? 'm.and customers for' : 'm.customers for',
             opts: { delayStart: i === order.length - 1 ? 0 : 200 },
           },
           ...this.pluraliseAudio(plats, {

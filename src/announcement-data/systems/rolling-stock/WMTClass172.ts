7a0e3878653f5bd79a3b4fcb9195b378a8a05d65
David Wheatley <david@davwheat.dev>
2025-04-28 23:01:54 +0100

    chore: format

--- src/announcement-data/systems/rolling-stock/WMTClass172.ts
+++ src/announcement-data/systems/rolling-stock/WMTClass172.ts
@@ -6,22 +6,22 @@ import { AudioItem, AudioItemObject, CustomAnnouncementTab } from '../../Announc
 import TrainAnnouncementSystem from '../../TrainAnnouncementSystem'
 
 interface IApproachingStationAnnouncementOptions {
-    stationCode: string
-    mindTheGap: boolean
+  stationCode: string
+  mindTheGap: boolean
 }
-  
+
 interface IWelcomeAnnouncementOptions {
-    terminatesAtCode: string
-    readAllStations: boolean
-    callingAtCodes: { crsCode: string; name: string; randomId: string }[]
+  terminatesAtCode: string
+  readAllStations: boolean
+  callingAtCodes: { crsCode: string; name: string; randomId: string }[]
 }
 
 const announcementPresets: Readonly<Record<string, ICustomAnnouncementPreset[]>> = {
   welcome: [
     {
-      name: 'Birmingham Snow Hill to Worcester Forgate Street',
+      name: 'Reddich to Four Oaks',
       state: {
-        terminatesAtCode: 'WOF',
+        terminatesAtCode: 'FOK',
         readAllStations: true,
         callingAtCodes: ['JEQ', 'THW', 'SGB', 'ROW', 'CRA', 'SBJ', 'HAG', 'BKD', 'KID', 'HBY', 'DTW'].map(crsToStationItemMapper),
       },
@@ -30,148 +30,147 @@ const announcementPresets: Readonly<Record<string, ICustomAnnouncementPreset[]>>
 }
 
 export default class WMTClass172 extends TrainAnnouncementSystem {
-    readonly NAME = 'West Midlands Trains Class 172 - Julie Berry'
-    readonly ID = 'WMT_CLASS_172_V1'
-    readonly FILE_PREFIX = 'WMT/172'
-    readonly SYSTEM_TYPE = 'train'
+  readonly NAME = 'West Midlands Trains Class 172 - Julie Berry'
+  readonly ID = 'WMT_CLASS_172_V1'
+  readonly FILE_PREFIX = 'WMT/172'
+  readonly SYSTEM_TYPE = 'train'
 
-    private async playApproachingStationAnnouncement(options: IApproachingStationAnnouncementOptions, download: boolean = false): Promise<void> {
-        const files: AudioItem[] = []
+  private async playApproachingStationAnnouncement(options: IApproachingStationAnnouncementOptions, download: boolean = false): Promise<void> {
+    const files: AudioItem[] = []
 
-        files.push('bing bong')
-        files.push('we are now approaching')
-        files.push({ id: `stations.${options.stationCode}`, opts: { delayStart: 200 } })
+    files.push('bing bong')
+    files.push('we are now approaching')
+    files.push({ id: `stations.${options.stationCode}`, opts: { delayStart: 200 } })
 
-        if (options.mindTheGap) {
-            files.push({ id: 'please mind the gap when leaving the train and step', opts: { delayStart: 500 } })
-        }
-
-        await this.playAudioFiles(files, download)
+    if (options.mindTheGap) {
+      files.push({ id: 'please mind the gap when leaving the train and step', opts: { delayStart: 500 } })
     }
 
-    private async playStoppedAtStationAnnouncement(options: IWelcomeAnnouncementOptions, download: boolean = false): Promise<void> {
-        const { callingAtCodes: _callingAt, terminatesAtCode, readAllStations } = options
+    await this.playAudioFiles(files, download)
+  }
 
-        const callingAtCodes = _callingAt.map(stop => stop.crsCode)
+  private async playStoppedAtStationAnnouncement(options: IWelcomeAnnouncementOptions, download: boolean = false): Promise<void> {
+    const { callingAtCodes: _callingAt, terminatesAtCode, readAllStations } = options
 
-        if (!this.validateStationExists(terminatesAtCode)) return
+    const callingAtCodes = _callingAt.map(stop => stop.crsCode)
 
-        const files: AudioItem[] = []
-        files.push('bing bong')
-        files.push('welcome to this service')
-        files.push({ id: 'for', opts: { delayStart: 200 } })
-        files.push({ id: `stations.${terminatesAtCode}`, opts: { delayStart: 200 } })
+    if (!this.validateStationExists(terminatesAtCode)) return
 
-        const remainingStops = [
-            ...callingAtCodes.map((crsCode): AudioItemObject => ({ id: `stations.${crsCode}`, opts: { delayStart: 100 } })),
-            { id: `stations.${terminatesAtCode}`, opts: { delayStart: 100 } },
-        ]
+    const files: AudioItem[] = []
+    files.push('bing bong')
+    files.push('welcome to this service')
+    files.push({ id: 'for', opts: { delayStart: 200 } })
+    files.push({ id: `stations.${terminatesAtCode}`, opts: { delayStart: 200 } })
 
-        if (callingAtCodes.some(code => !this.validateStationExists(code))) return
-
-        if (remainingStops.length === 0) {
-            // We are at the termination point.
-            // files.push('this train terminates here all change please ensure')
-        } else if (remainingStops.length === 1 || !readAllStations) {
-            // Next station is the termination point or we are not reading all stations.
-            files.push({ id: `the next station is`, opts: { delayStart: 200 } })
-            files.push(remainingStops[0])
-        } else {
-            // We are not at the termination point and reading all stations.
-            files.push({ id: `calling at`, opts: { delayStart: 200 } })
-            files.push(...this.pluraliseAudio(remainingStops, { beforeAndDelay: 200, beforeItemDelay: 200 }))
-        }
-
-        await this.playAudioFiles(files, download)
-    }
-
-    private RealAvailableStationNames = [
-        'ACG',
-        'BKD',
-        'BMO',
-        'BSW',
-        'CRA',
-        'DTW',
-        'HAG',
-        'HBY',
-        'JEQ',
-        'KID',
-        'OLT',
-        'ROW',
-        'SBJ',
-        'SGB',
-        'SMA',
-        'THW',
-        'WOF',
+    const remainingStops = [
+      ...callingAtCodes.map((crsCode): AudioItemObject => ({ id: `stations.${crsCode}`, opts: { delayStart: 100 } })),
+      { id: `stations.${terminatesAtCode}`, opts: { delayStart: 100 } },
     ]
 
-    readonly AvailableStationNames = {
-        high: this.RealAvailableStationNames,
-        low: this.RealAvailableStationNames,
+    if (callingAtCodes.some(code => !this.validateStationExists(code))) return
+
+    if (remainingStops.length === 0) {
+      // We are at the termination point.
+      // files.push('this train terminates here all change please ensure')
+    } else if (remainingStops.length === 1 || !readAllStations) {
+      // Next station is the termination point or we are not reading all stations.
+      files.push({ id: `the next station is`, opts: { delayStart: 200 } })
+      files.push(remainingStops[0])
+    } else {
+      // We are not at the termination point and reading all stations.
+      files.push({ id: `calling at`, opts: { delayStart: 200 } })
+      files.push(...this.pluraliseAudio(remainingStops, { beforeAndDelay: 200, beforeItemDelay: 200 }))
     }
 
-    readonly customAnnouncementTabs: Record<string, CustomAnnouncementTab<string>> = {
-        approachingStation: {
-            name: 'Approaching station',
-            component: CustomAnnouncementPane,
-            defaultState: {
-                stationCode: this.RealAvailableStationNames[0],
-                mindTheGap: true,
-                keepBelongings: false,
-                cannotUseOyster: false,
-            },
+    await this.playAudioFiles(files, download)
+  }
+
+  private RealAvailableStationNames = [
+    'ACG',
+    'BKD',
+    'BMO',
+    'BSW',
+    'CRA',
+    'DTW',
+    'HAG',
+    'HBY',
+    'JEQ',
+    'KID',
+    'OLT',
+    'ROW',
+    'SBJ',
+    'SGB',
+    'SMA',
+    'THW',
+    'WOF',
+  ]
+
+  readonly AvailableStationNames = {
+    high: this.RealAvailableStationNames,
+    low: this.RealAvailableStationNames,
+  }
+
+  readonly customAnnouncementTabs: Record<string, CustomAnnouncementTab<string>> = {
+    approachingStation: {
+      name: 'Approaching station',
+      component: CustomAnnouncementPane,
+      defaultState: {
+        stationCode: this.RealAvailableStationNames[0],
+        mindTheGap: true,
+        keepBelongings: false,
+        cannotUseOyster: false,
+      },
+      props: {
+        playHandler: this.playApproachingStationAnnouncement.bind(this),
+        options: {
+          stationCode: {
+            name: 'Next station',
+            default: this.RealAvailableStationNames[0],
+            options: AllStationsTitleValueMap.filter(s => this.RealAvailableStationNames.includes(s.value)),
+            type: 'select',
+          },
+          mindTheGap: {
+            name: 'Mind the gap?',
+            type: 'boolean',
+            default: true,
+          },
+        },
+      },
+    } as CustomAnnouncementTab<keyof IApproachingStationAnnouncementOptions>,
+    stoppedAtStation: {
+      name: 'Stopped at station',
+      component: CustomAnnouncementPane,
+      defaultState: {
+        terminatesAtCode: this.RealAvailableStationNames[0],
+        readAllStations: true,
+        callingAtCodes: [],
+      },
+      props: {
+        playHandler: this.playStoppedAtStationAnnouncement.bind(this),
+        presets: announcementPresets.welcome,
+        options: {
+          terminatesAtCode: {
+            name: 'Terminates at',
+            default: this.RealAvailableStationNames[0],
+            options: AllStationsTitleValueMap.filter(s => this.RealAvailableStationNames.includes(s.value)),
+            type: 'select',
+          },
+          readAllStations: {
+            name: 'Read all stations?',
+            type: 'boolean',
+            default: true,
+          },
+          callingAtCodes: {
+            name: '',
+            type: 'custom',
+            component: CallingAtSelector,
             props: {
-                playHandler: this.playApproachingStationAnnouncement.bind(this),
-                options: {
-                    stationCode: {
-                        name: 'Next station',
-                        default: this.RealAvailableStationNames[0],
-                        options: AllStationsTitleValueMap.filter(s => this.RealAvailableStationNames.includes(s.value)),
-                        type: 'select',
-                    },
-                    mindTheGap: {
-                        name: 'Mind the gap?',
-                        type: 'boolean',
-                        default: true,
-                    },
-                },
+              availableStations: this.RealAvailableStationNames,
             },
-        } as CustomAnnouncementTab<keyof IApproachingStationAnnouncementOptions>,
-        stoppedAtStation: {
-            name: 'Stopped at station',
-            component: CustomAnnouncementPane,
-            defaultState: {
-                terminatesAtCode: this.RealAvailableStationNames[0],
-                readAllStations: true,
-                callingAtCodes: [],
-            },
-            props: {
-                playHandler: this.playStoppedAtStationAnnouncement.bind(this),
-                presets: announcementPresets.welcome,
-                options: {
-                    terminatesAtCode: {
-                        name: 'Terminates at',
-                        default: this.RealAvailableStationNames[0],
-                        options: AllStationsTitleValueMap.filter(s => this.RealAvailableStationNames.includes(s.value)),
-                        type: 'select',
-                    },
-                    readAllStations: {
-                        name: 'Read all stations?',
-                        type: 'boolean',
-                        default: true,
-                    },
-                    callingAtCodes: {
-                        name: '',
-                        type: 'custom',
-                        component: CallingAtSelector,
-                        props: {
-                          availableStations: this.RealAvailableStationNames,
-                        },
-                        default: [],
-                      },
-                },
-            },
-        } as CustomAnnouncementTab<keyof IWelcomeAnnouncementOptions>,
-    }
+            default: [],
+          },
+        },
+      },
+    } as CustomAnnouncementTab<keyof IWelcomeAnnouncementOptions>,
+  }
 }
-  
